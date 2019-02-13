81067931396052652b6fd70c336c5227e1460850
Natasha Chernis <natashachernis@gmail.com>
2019-02-13 04:51:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(start_time)
+  while start_time >= 0
+    puts "#{start_time} SECOND(S)!"
+    start_time -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+countdown(10)
