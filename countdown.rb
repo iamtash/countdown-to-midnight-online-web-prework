be704680bfe35d8f2b1d16d39d1b16444c489823
Natasha Chernis <natashachernis@gmail.com>
2019-02-13 05:05:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(start_time)
+  while start_time > 0
+    puts "#{start_time} SECOND(S)!"
+    start_time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(start_time)
+  while start_time >= 0
+    puts "#{start_time} SECOND(S)!"
+    sleep(1)
+    start_time -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
