c6aaf0facf53af0b36e4f767feeb4d17ded1b2f3
Natasha Chernis <natashachernis@gmail.com>
2019-02-13 04:56:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,12 @@ def countdown(start_time)
   "HAPPY NEW YEAR!"
 end
 
-countdown(10)
+
+def countdown_with_sleep(start_time)
+  while start_time >= 0
+    puts "#{start_time} SECOND(S)!"
+    sleep(1)
+    start_time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
